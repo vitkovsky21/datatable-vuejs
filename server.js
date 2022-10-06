2d171b25fe1ea22b75a3c33bb7a7e566ad81e611
vitkovsky21 <vladbekkoshkarov@yandex.ru>
2022-10-06 21:16:05 +0300

    cors forced remove

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const path = require('path');
 
 const app = express();
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
